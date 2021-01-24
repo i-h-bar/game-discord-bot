75e8d1f98ba5a9d302178ec26dcc94d8d3534032
Joe Wasden <jpwasden@gmail.com>
2021-01-24 10:34:02 +0000

    Added cache to async_get using async-cache package

--- requests_handler/async_requests.py
+++ requests_handler/async_requests.py
@@ -1,7 +1,9 @@
 import httpx
+from cache import AsyncTTL
 from httpx import Timeout
 
 
+@AsyncTTL(time_to_live=86400, min_cleanup_interval=3600)
 async def async_get(url: str, return_json: bool = True):
     async with httpx.AsyncClient(timeout=Timeout(timeout=10.0)) as client:
         raw_response = await client.get(url)
