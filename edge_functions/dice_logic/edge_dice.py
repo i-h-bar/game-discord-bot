2e6c687e8512d921bbb1a82037cde4a39c4380cd
Joe Wasden <jpwasden@gmail.com>
2021-07-13 19:51:57 +0100

    Added total calculation to edge die

--- edge_functions/dice_logic/edge_dice.py
+++ edge_functions/dice_logic/edge_dice.py
@@ -13,16 +13,19 @@ class EdgeDie:
 
 class Boost(EdgeDie):
     name = "Boost Die"
+    positive = True
     sides = [BLANK, BLANK, SUCCESS, SUCCESS + ADVANTAGE, ADVANTAGE * 2, ADVANTAGE]
 
 
 class SetBack(EdgeDie):
     name = "Setback Die"
+    positive = False
     sides = [BLANK] * 2 + [FAILURE] * 2 + [THREAT] * 2
 
 
 class Ability(EdgeDie):
     name = "Ability Die"
+    positive = True
     sides = [
         BLANK, SUCCESS, SUCCESS, SUCCESS * 2, ADVANTAGE, ADVANTAGE, SUCCESS + ADVANTAGE, ADVANTAGE * 2
     ]
@@ -30,17 +33,20 @@ class Ability(EdgeDie):
 
 class Difficulty(EdgeDie):
     name = "Difficulty Die"
+    positive = False
     sides = [BLANK, FAILURE, FAILURE * 2] + [THREAT] * 3 + [THREAT * 2, FAILURE + THREAT]
 
 
 class Proficiency(EdgeDie):
     name = "Proficiency Die"
+    positive = True
     sides = [BLANK, SUCCESS, SUCCESS, SUCCESS * 2, SUCCESS * 2, ADVANTAGE, TRIUMPH, ADVANTAGE * 2, ADVANTAGE * 2] + [
         SUCCESS + ADVANTAGE] * 3
 
 
 class Challenge(EdgeDie):
     name = "Challenge Die"
+    positive = False
     sides = [
         FAILURE, FAILURE, FAILURE * 2, FAILURE * 2, THREAT, THREAT,
         FAILURE + THREAT, FAILURE + THREAT, THREAT * 2, THREAT * 2, DESPAIR
@@ -49,4 +55,5 @@ class Challenge(EdgeDie):
 
 class Force(EdgeDie):
     name = "Force Die"
+    positive = None
     sides = [DARK]*6 + [DARK*2] + [LIGHT]*2 + [LIGHT*2]*3
