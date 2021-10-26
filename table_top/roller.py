1a935c73541b609c3e81504ad52c58d40b88bb79
Joe Wasden <jpwasden@gmail.com>
2021-10-26 19:35:06 +0100

    Added repeated action rolling back into roll classic dice

--- table_top/roller.py
+++ table_top/roller.py
@@ -1,3 +1,5 @@
+import re
+
 from edge.dice.command_map import dice_map
 from edge.dice.roller import roll_edge_dice
 from table_top.dice import Dice
@@ -12,9 +14,25 @@ def get_roll(message):
     else:
         content = content.lstrip("/rol ")
 
-        dice_pool = [int(dice) if dice.strip().isdigit() else Dice.from_message(dice) for dice in content.split("+")]
+        if "[" in content and "]" in content:
+            message = '\n'.join(
+                roll_classic(dice.lstrip('[').rstrip(']'))
+                for group in content.split(",")
+                for num, dice in re.findall(r'(\d+)(\[.*])', group.strip())
+                for _ in range(int(num))
+            )
+            return f"\n{message}"
 
-        return (
-            f"`{' + '.join(repr(dice) for dice in dice_pool)}` = "
-            f"{' + '.join(str(dice) for dice in dice_pool)} = {sum(dice_pool)}"
-        )
+        else:
+            return roll_classic(content)
+
+
+def roll_classic(content):
+    dice_pool = [
+        int(dice) if dice.strip().isdigit() else Dice.from_message(dice) for dice in content.split("+")
+    ]
+
+    return (
+        f"`{' + '.join(repr(dice) for dice in dice_pool)}` = "
+        f"{' + '.join(str(dice) for dice in dice_pool)} = {sum(dice_pool)}"
+    )
