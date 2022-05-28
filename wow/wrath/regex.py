65317c641b11c64209556f240b2add7b0d1b1339
Joe Wasden <jpwasden@gmail.com>
2022-05-28 09:41:37 +0100

    Updated wrath polling regex to include punctuation

--- wow/wrath/regex.py
+++ wow/wrath/regex.py
@@ -1,1 +1,4 @@
-poll_regex = r"(?m)^1.[a-zA-Z ]+(?:\n(30|[12][0-9]|[2-9])\.[a-zA-Z ]+)+"
+from string import punctuation
+
+poll_regex = fr"(?m)^1.[a-zA-Z {punctuation}]+(?:\n(30|[12][0-9]|[1-9])\.[a-zA-Z {punctuation}]+)+"
+x = 0
