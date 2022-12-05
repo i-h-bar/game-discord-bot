7951b711442a963a2de1a10abbe7610a20eb785d
Joe Wasden <jpwasden@gmail.com>
2022-12-05 23:06:12 +0000

    Updated wow help msg

--- wow/help_message.py
+++ wow/help_message.py
@@ -7,11 +7,11 @@ WOW_HELP = "`/search item_name [hide]` - A search function to search for an item
            "\n" \
            "`/item item_name [hide]` - A search function to search for an item. This fuzzy matches so you can " \
            "misspell the name and the bot will match it to an in game item the best it can." \
-           "\nExample : `/search name:dirge`.\nYou can hide this result from other users if you wish using the" \
+           "\nExample : `/item name:dirge`.\nYou can hide this result from other users if you wish using the" \
            " `hide` argument either Yes or No (This is optional but will not hide by default)." \
            "\n" \
            "\n" \
            "`/spell item_name [hide]` - A search function to search for an item. This fuzzy matches so you can " \
            "misspell the name and the bot will match it to an in game spell the best it can." \
-           "\nExample : `/search name:frostbolt`.\nYou can hide this result from other users if you wish using the" \
+           "\nExample : `/spell name:frostbolt`.\nYou can hide this result from other users if you wish using the" \
            " `hide` argument either Yes or No (This is optional but will not hide by default)."
